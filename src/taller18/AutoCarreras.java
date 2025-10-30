6d0cb359df2305f7d39545216aba4ea0c7897a07
mapeflo <mpenarandaf@unicartagena.edu.co>
2025-10-30 14:10:12 -0500

    Avance taller 18-ejercicio 3

---
+++ src/taller18/AutoCarreras.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller18;
+
+/**
+ *
+ * @author marya
+ */
+public class AutoCarreras implements Conduccion {
+    
+    @Override
+    public void conducir(){
+        System.out.println("El auto de carreras marcha hasta mas de 300km/h");
+    }   
+}
