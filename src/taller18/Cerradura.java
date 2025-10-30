9646c1e0d432ced41a0f1a77fa0d17095312bde9
mapeflo <mpenarandaf@unicartagena.edu.co>
2025-10-30 00:46:40 -0500

    Avance taller 18-ejercicio 1

---
+++ src/taller18/Cerradura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller18;
+
+/**
+ *
+ * @author marya
+ */
+public class Cerradura implements Reparaciones{
+    
+    @Override
+    public void reparar(){
+        System.out.println("La cerradura esta siendo reparada");
+    }
+}
---
+++ src/taller18/Vehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller18;
+
+/**
+ *
+ * @author marya
+ */
+public class Vehiculo implements Limpieza {
+    
+    @Override
+    public void limpiar(){
+        System.out.println("El vehiculo se esta lavando");
+    }
+}
